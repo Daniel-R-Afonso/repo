a4c74e9ea571b34f2e6a3c8f86b44106cb892115
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2015-06-03 17:54:40 +0100

    para o yoko

---
+++ WebApplication1/Scripts/layout.js
@@ -0,0 +1,9 @@
+﻿
+$(document).ready(function () {
+    $(".1").hover(function () {
+        $("p").hide();
+    });
+    $(".menu-1").click(function () {
+        $("#menu-2-1").text("Hello world!");
+    });
+});
