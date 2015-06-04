8a72f6700f5c2eb8d9bc815c9f1c6ece3f65a726
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2015-06-04 16:11:11 +0100

    metade das aulas

--- WebApplication1/Scripts/layout.js
+++ WebApplication1/Scripts/layout.js
@@ -24,13 +24,20 @@ $(document).ready(function () {
         var txt3 = $("<button type='button' class='btn btn-success'>Ver</button>");
         $(".index-click").append(txt1, txt2,txt3);
     });
+
+    //cenas dos mapas
+
+    cvi_map.defaultRadius = 0;	//INT 0-100 (px radius)
+    cvi_map.defaultOpacity = 33; //INT 0-100 (% opacity)
+    cvi_map.defaultBordercolor = '#0000ff'; //STR '#000000'-'#ffffff'
+    cvi_map.defaultAreacolor = '#000000'; //STR '#000000'-'#ffffff'
+    cvi_map.defaultNoborder = false; //BOOLEAN
+    cvi_map.defaultNofade = false; //BOOLEAN
+    cvi_map.defaultShowcoords = false; //BOOLEAN
+    cvi_map.defaultDelayed = false;	//BOOLEAN
+    cvi_map.defaultImgsrc = ''; //STR (path&file)
+    cvi_map.defaultMapid = ''; //STR (id)
+
+    cvi_map.add(document.getElementById("mapa_portugal"), { opacity: 25, areacolor: 'ff0000' });
 });
 
-function myFunc(id) {
-
-    $(".cenas").empty();
-    var txt1 = $("<p class='text-center'></p>").text("Vê como te tens safado");
-    var txt2 = $("<p class='text-center'></p>").text("E mostra aos teus amigos!");
-    var txt3 = $("<button type='button' class='btn btn-success'>Ver</button>");
-    $(".cenas").append(txt1, txt2, txt3);
-}
